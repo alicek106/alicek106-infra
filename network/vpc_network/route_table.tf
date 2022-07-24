4564515b38b78e8a62971595e0885ec8b326f53a
alicek106 <alice_k106@naver.com>
2022-07-24 15:48:16 +0900

    Add comment to private subnet route table for NAT Gateway

--- network/vpc_network/route_table.tf
+++ network/vpc_network/route_table.tf
@@ -20,6 +20,7 @@ resource "aws_route_table" "private_route_table" {
   }, var.common_tags)
 }
 
+// NAT Gateway는 EIP + 추가비용이 나가기 때문에 상시 띄워놓지는 않는다. 필요할 경우에만 만들어서 쓰고 지울 것.
 resource "aws_route" "private_route_igw" {
   route_table_id         = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
