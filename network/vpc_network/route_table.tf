d8b838736e92f0290e343a32cf7c11363cd9aaa8
alicek106 <alice_k106@naver.com>
2024-02-05 21:16:22 +0900

    network: fix error when disabled nat gateway

--- network/vpc_network/route_table.tf
+++ network/vpc_network/route_table.tf
@@ -24,5 +24,6 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route" "private_route_igw" {
   route_table_id         = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = var.enable_nat_gateway ? aws_nat_gateway.main[0].id : aws_internet_gateway.main.id
+  nat_gateway_id         = var.enable_nat_gateway ? aws_nat_gateway.main[0].id : null
+  gateway_id             = var.enable_nat_gateway ? null : aws_internet_gateway.main.id
 }
